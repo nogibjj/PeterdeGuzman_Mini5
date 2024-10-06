2d63402f098192b1b2e4f256d58ed1df803a43c2
PeterdeGuzman <pdeguz01@peters-mbp.lan>
2024-10-06 18:29:00 -0400

    updating

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -57,49 +57,49 @@ def load(dataset="./data/pollingplaces_2020.csv"):
 # saved copy
 
 
-def load(dataset="./data/pollingplaces_2020.csv"):
-    payload = csv.reader(open(dataset, newline="", encoding="utf-16"), delimiter="\t")
-    conn = sqlite3.connect("pollingplaces_2020.db")
-    c = conn.cursor()
-    # generate new table for the database
-    c.execute("DROP TABLE IF EXISTS pollingplaces_2020")
-    c.execute(
-        """
-            CREATE TABLE pollingplaces_2020 (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            election_dt DATE,
-            county_name TEXT, 
-            polling_place_id INTEGER,
-            polling_place_name TEXT,
-            precinct_name TEXT,
-            house_num INTEGER,
-            street_name TEXT, 
-            city TEXT,
-            state TEXT,
-            zip TEXT)
-            """
-    )
-    # insert values
-    c.executemany(
-        """
-            INSERT INTO pollingplaces_2020 (
-            election_dt, 
-            county_name,
-            polling_place_id,
-            polling_place_name,
-            precinct_name, 
-            house_num,
-            street_name,
-            city,
-            state,
-            zip)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) 
-            """,
-        payload,
-    )
-    conn.commit()
-    conn.close()
-    return "pollingplaces_2020.db"
+# def load(dataset="./data/pollingplaces_2020.csv"):
+#     payload = csv.reader(open(dataset, newline="", encoding="utf-16"), delimiter="\t")
+#     conn = sqlite3.connect("pollingplaces_2020.db")
+#     c = conn.cursor()
+#     # generate new table for the database
+#     c.execute("DROP TABLE IF EXISTS pollingplaces_2020")
+#     c.execute(
+#         """
+#             CREATE TABLE pollingplaces_2020 (
+#             id INTEGER PRIMARY KEY AUTOINCREMENT,
+#             election_dt DATE,
+#             county_name TEXT,
+#             polling_place_id INTEGER,
+#             polling_place_name TEXT,
+#             precinct_name TEXT,
+#             house_num INTEGER,
+#             street_name TEXT,
+#             city TEXT,
+#             state TEXT,
+#             zip TEXT)
+#             """
+#     )
+#     # insert values
+#     c.executemany(
+#         """
+#             INSERT INTO pollingplaces_2020 (
+#             election_dt,
+#             county_name,
+#             polling_place_id,
+#             polling_place_name,
+#             precinct_name,
+#             house_num,
+#             street_name,
+#             city,
+#             state,
+#             zip)
+#             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+#             """,
+#         payload,
+#     )
+#     conn.commit()
+#     conn.close()
+#     return "pollingplaces_2020.db"
 
 
 if __name__ == "__main__":
