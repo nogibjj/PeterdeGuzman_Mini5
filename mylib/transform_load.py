08d197c3c16fdb94c2cdc4b1ae9ff31cd9bc0c53
PeterdeGuzman <pdeguz01@peters-mbp.lan>
2024-10-06 18:25:06 -0400

    removing null bytes

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -5,6 +5,57 @@ Transform and load with SQlite3 database
 import sqlite3
 import csv
 
+# test to replace null bytes
+
+
+def load(dataset="./data/pollingplaces_2020.csv"):
+    payload = csv.reader(open(dataset, newline="", encoding="utf-16"), delimiter="\t")
+    payload_withoutnull = ["" if item == b"\x00" else item for item in payload]
+    conn = sqlite3.connect("pollingplaces_2020.db")
+    c = conn.cursor()
+    # generate new table for the database
+    c.execute("DROP TABLE IF EXISTS pollingplaces_2020")
+    c.execute(
+        """
+            CREATE TABLE pollingplaces_2020 (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            election_dt DATE,
+            county_name TEXT, 
+            polling_place_id INTEGER,
+            polling_place_name TEXT,
+            precinct_name TEXT,
+            house_num INTEGER,
+            street_name TEXT, 
+            city TEXT,
+            state TEXT,
+            zip TEXT)
+            """
+    )
+    # insert values
+    c.executemany(
+        """
+            INSERT INTO pollingplaces_2020 (
+            election_dt, 
+            county_name,
+            polling_place_id,
+            polling_place_name,
+            precinct_name, 
+            house_num,
+            street_name,
+            city,
+            state,
+            zip)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) 
+            """,
+        payload_withoutnull,
+    )
+    conn.commit()
+    conn.close()
+    return "pollingplaces_2020.db"
+
+
+# saved copy
+
 
 def load(dataset="./data/pollingplaces_2020.csv"):
     payload = csv.reader(open(dataset, newline="", encoding="utf-16"), delimiter="\t")
